92a1e5fcb99c5a5a49b1152e033eeb461c925942
JakubZebrowski <49155415+jakubzebrowski98@users.noreply.github.com>
2020-09-09 21:30:28 +0200

    c# paint

---
+++ paint/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 403);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Down";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(295, 403);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Left";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(425, 403);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 25);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Right";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(685, 403);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 25);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(21, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(750, 350);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(801, 453);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ paint/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        int x= 150;
+        int y = 350;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+            Graphics nowy = panel1.CreateGraphics();
+            SolidBrush pen1 = new SolidBrush(Color.Green);
+            nowy.FillRectangle(pen1,y , x-25 , 25, 25);
+            x -= 25;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graphics nowy = panel1.CreateGraphics();
+            SolidBrush pen1 = new SolidBrush(Color.Green);
+            nowy.FillRectangle(pen1, y, x+25, 25, 25);
+            x += 25;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Graphics nowy = panel1.CreateGraphics();
+            SolidBrush pen1 = new SolidBrush(Color.Green);
+            nowy.FillRectangle(pen1, y-25, x, 25, 25);
+            y -= 25;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Graphics nowy = panel1.CreateGraphics();
+            SolidBrush pen1 = new SolidBrush(Color.Green);
+            nowy.FillRectangle(pen1, y + 25, x, 25, 25);
+            y += 25;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+             x = 150;
+             y = 350;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+    }
+}
