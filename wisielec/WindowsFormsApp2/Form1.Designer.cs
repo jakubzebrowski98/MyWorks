c1959d26be6aeac63bce644d6e9e9eb75333a692
JakubZebrowski <49155415+jakubzebrowski98@users.noreply.github.com>
2020-09-09 21:29:25 +0200

    C# gra w wisielca

---
+++ wisielec/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox1.Location = new System.Drawing.Point(21, 222);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(83, 38);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "sprawdź";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.label1.Location = new System.Drawing.Point(18, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 52);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wisielec";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(254, 30);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 31);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Wylosuj haslo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.label3.Location = new System.Drawing.Point(223, 287);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 76);
+            this.label3.TabIndex = 5;
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
+            this.label5.Location = new System.Drawing.Point(223, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 113);
+            this.label5.TabIndex = 7;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.label6.Location = new System.Drawing.Point(244, 172);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 58);
+            this.label6.TabIndex = 8;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 60F);
+            this.label7.Location = new System.Drawing.Point(262, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 113);
+            this.label7.TabIndex = 9;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label9.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label9.Location = new System.Drawing.Point(336, 175);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 31);
+            this.label9.TabIndex = 11;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label10.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label10.Location = new System.Drawing.Point(332, 203);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 31);
+            this.label10.TabIndex = 12;
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label11.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label11.Location = new System.Drawing.Point(328, 229);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 31);
+            this.label11.TabIndex = 13;
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label12.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label12.Location = new System.Drawing.Point(332, 252);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(0, 31);
+            this.label12.TabIndex = 14;
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(416, 278);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Wisielec";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ wisielec/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        static char[] tab;
+        string podana;
+        string haslo,bierzaca;
+        int ilosc, i, wylosowana;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            podana = textBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            podana = textBox1.Text;
+            int y = 0;
+
+            label1.Text = "";
+
+            for (i = 0; i < ilosc; i++)
+            {
+                if (podana[0] == haslo[i])
+                    tab[i] = podana[0];
+            }
+            for (i = 0; i < ilosc; i++)
+            {
+                label1.Text += " " + tab[i] + " ";
+            }
+            
+            if (tab.ToString() != haslo)
+                y++;
+
+
+            if (y == 1)
+            {
+                label3.Text = "/\\";
+                
+            }
+            if (y == 2)
+            {
+                label5.Text = "|";
+               
+            }
+            if (y == 3)
+            {
+                label6.Text = "/";
+               
+            }
+            if (y == 4)
+            {
+                label7.Text = "_";
+             
+            }
+            if (y == 5)
+            {
+                label9.Text = "|";
+        
+            }
+            if (y == 6)
+            {
+                label10.Text = "O";
+          
+            }
+            if (y == 7)
+            {
+                label11.Text = "/|\\";
+ 
+            }
+            if (y == 8)
+            {
+                label12.Text = "/\\";
+          
+            }
+
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+            wylosowana = random.Next(0, 11);
+            string[] linia = System.IO.File.ReadAllLines(@"../../../wyrazy.txt");
+            haslo = linia[wylosowana];
+            ilosc = haslo.Length;
+            tab = new char[ilosc];
+            for (i = 0; i < ilosc; i++)
+            {
+                tab[i] = '_';
+            }
+            for (i = 0; i < ilosc; i++)
+            {
+                label1.Text +=  " " + tab[i] + " ";
+            }
+        }
+    }
+}
